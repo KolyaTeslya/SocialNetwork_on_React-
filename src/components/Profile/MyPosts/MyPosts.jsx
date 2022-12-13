f7a0f48ad56d038df1386d673c827acb64e33fbf
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-12-13 20:24:47 +0200

    add new component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import s from '../MyPosts/MyPosts.module';
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
 
 const MyPosts = () => {
-  return 
-        <div>
-          My posts
-          <div>
-            New post 
-          </div>
-          <div className={s.posts}>
-            <div className={s.item}>
-              post 1
-            </div>
-            <div className="item">
-              post 2
-            </div>
-          </div>
-        </div>
+  return (
+    <div>
+      My posts
+      <div>
+        <textarea></textarea>
+        <button> Add post</button>
+      </div>
+      <div className={s.posts}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 };
 
 export default MyPosts;
