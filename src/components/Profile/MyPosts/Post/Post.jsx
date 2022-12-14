8f6618897ffb96cb09d45492a7982738284cac15
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-12-14 19:42:45 +0200

    created new props

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.item}>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5X1bnwLsxFXgAyBY00Kujl6PBKJ5t9fbVkg&usqp=CAU" />
-      post 1
+      {props.message}
       <div>
         <span>like</span>
       </div>
