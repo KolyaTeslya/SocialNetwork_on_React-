73bdf08816dc253395087c60014018201a6d3893
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-12-15 20:28:18 +0200

    Added new component

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ const Post = (props) => {
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5X1bnwLsxFXgAyBY00Kujl6PBKJ5t9fbVkg&usqp=CAU" />
       {props.message}
       <div>
-        <span>like</span>
+        <span>like</span> {props.likesCount}
       </div>
     </div>
   );
