87abcd37572bef8e6c7b71580fddf0ca314731e7
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-12-17 13:27:16 +0200

    added new routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import s from "./Navbar.module.css";
-console.log(s);
-// let s = {
-//   nav: "Navbar_nav__DS0RX",
-//   item: "Navbar_item__Ccd2B",
-// };
 
-let c1 = "item";
-let c2 = "active";
-// item active
-let classes = c1 + " " + c2;
-let classesNew = `${s.item} ${s.active}`;
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a>Profile</a>
+        <a href="/profile">Profile</a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a>Messages</a>
+        <a href="/dialogs">Messages</a>
       </div>
       <div className={s.item}>
         <a>News</a>
