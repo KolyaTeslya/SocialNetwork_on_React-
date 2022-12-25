82c63655fc0faa43df659c44152ebd6a5ab726cd
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-12-25 20:24:13 +0200

    added redux

---
+++ src/redux/state.js
@@ -0,0 +1,31 @@
+let state = {
+    profilePage : {
+        posts: [
+            { id: 1, message: "Hi, how are you?", likesCount: 12 },
+            { id: 2, message: "It's my first post", likesCount: 11 },
+            { id: 3, message: "Ok", likesCount: 14 },
+            { id: 4, message: "It's cool", likesCount: 17 },
+          ]
+    },
+    dialogsPage: {
+        dialogs: [
+            { id: 1, name: "Kolya" },
+            { id: 2, name: "Vadym" },
+            { id: 3, name: "Sasha" },
+            { id: 4, name: "Dima" },
+            { id: 5, name: "Viktor" },
+            { id: 6, name: "Valera" },
+          ],
+        messages: [
+            { id: 1, message: "Hi" },
+            { id: 2, message: "How are you?" },
+            { id: 3, message: "Yo" },
+            { id: 4, message: "Yo" },
+            { id: 5, message: "Yo" },
+          ]
+    },
+    sidebar : {}
+
+}
+
+export default state;
