c8548f90a8e7fa48f11d8ed99d8da1500f6a92a7
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-01-09 23:22:09 +0200

    add reducers

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,9 +1,12 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+import sidebarReducer from "./sidebar-reducer";
 
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
-const SEND_MESSAGE = 'SEND-MESSAGE';
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
+const UPDATE_NEW_MESSAGE_BODY = "UPDATE_NEW_MESSAGE_BODY";
+const SEND_MESSAGE = "SEND_MESSAGE";
 
 let store = {
   _state: {
@@ -18,74 +21,58 @@ let store = {
     },
     dialogsPage: {
       dialogs: [
-        { id: 1, name: 'Kolya' },
-        { id: 2, name: 'Vadym' },
-        { id: 3, name: 'Sasha' },
-        { id: 4, name: 'Dima' },
-        { id: 5, name: 'Viktor' },
-        { id: 6, name: 'Valera' },
+        { id: 1, name: "Kolya" },
+        { id: 2, name: "Vadym" },
+        { id: 3, name: "Sasha" },
+        { id: 4, name: "Dima" },
+        { id: 5, name: "Viktor" },
+        { id: 6, name: "Valera" },
       ],
       messages: [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'How are you?' },
-        { id: 3, message: 'Yo' },
-        { id: 4, message: 'Yo' },
-        { id: 5, message: 'Yo' }
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Yo" },
+        { id: 4, message: "Yo" },
+        { id: 5, message: "Yo" },
       ],
-      newMessageBody: ""
+      newMessageBody: "",
     },
     sidebar: {},
   },
-  _callSubscriber()  {
+  _callSubscriber() {
     console.log("State changed");
   },
 
-  getState(){
+  getState() {
     return this._state;
   },
-  subscribe (observer) {
+  subscribe(observer) {
     this._callSubscriber = observer;
   },
 
+  dispatch(action) {
+    this._state.profilePage = profileReducer(this._state.profilePage, action);
+    this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+    this._state.sidebar = sidebarReducer(this._state.sidebar, action);
 
-  dispatch(action){ //  { type: 'ADD-POST' }
-    if (action.type === ADD_POST) {
-    let newPost = {
-      id: 5,
-      message: this._state.profilePage.newPostText,
-      likesCount: 0,
-    };
-    this._state.profilePage.posts.push(newPost);
-    this._state.profilePage.newPostText = "";
     this._callSubscriber(this._state);
-  } else if (action.type === UPDATE_NEW_POST_TEXT) {
-    this._state.profilePage.newPostText = action.newText;
-    this._callSubscriber(this._state);
-  } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-    this._state.dialogsPage.newMessageBody = action.body;
-    this._callSubscriber(this._state);
-  } else if (action.type === SEND_MESSAGE) {
-    let body = this._state.dialogsPage.newMessageBody; 
-    this._state.dialogsPage.newMessageBody = '';
-    this.dialogsPage.messages.push({ id: 6, message: body }); 
-    this._callSubscriber(this._state);
+
   }
-  }
+}
 
-};
+export const addPostActionCreator = () => ({ type: ADD_POST });
 
-export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
 
-export const updateNewPostTextActionCreator = (text) => 
-      ({type: UPDATE_NEW_POST_TEXT, newText: text })
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
 
-export const sendMessageCreator = () => ({type: SEND_MESSAGE})
-
-export const updateNewMessageBodyCreator = (body) => 
-      ({type: UPDATE_NEW_MESSAGE_BODY, body: body })
-
-
-
+export const updateNewMessageBodyCreator = (body) => ({
+  type: UPDATE_NEW_MESSAGE_BODY,
+  body: body,
+});
 
 export default store;
 
