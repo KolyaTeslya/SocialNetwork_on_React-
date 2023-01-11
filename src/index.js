1bc4215c1ac9fed97eabaa951b67bfab6943219e
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-01-11 19:32:08 +0200

    add context

--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/redux-store";
 
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "./StoreContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root')); 
-let rerenderEntireTree = (state) =>{
-    root.render(
-      <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let rerenderEntireTree = (state) => {
+  root.render(
+    <BrowserRouter>
+      <Provider value={store}>
         <React.StrictMode>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store}  />
+          <App />
         </React.StrictMode>
-        </BrowserRouter>
-    );
-  }
-
+      </Provider>
+    </BrowserRouter>
+  );
+};
 
 rerenderEntireTree(store.getState());
 
-store.subscribe( () => {
+store.subscribe(() => {
   let state = store.getState();
   rerenderEntireTree(state);
 });
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
