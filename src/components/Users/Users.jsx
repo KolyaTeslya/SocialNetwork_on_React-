c20cb968c91074121eb085465307a0c13de0e028
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-01-28 12:04:26 +0200

    button-disabled follow, unfollow

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -41,8 +41,8 @@ let Users = (props) => {
             </div>
             <div>
               {u.followed 
-              ? <button onClick={() => { 
-
+              ? <button disabled={props.followingInProgress.some(id=> id === u.id)} onClick={() => { 
+                  props.toggleFollowingProgress(true, u.id);
                 axios .delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
                   withCredentials:true,
                   headers: { 
@@ -53,13 +53,14 @@ let Users = (props) => {
                   if (response.data.resultCode === 0) {
                       props.unfollow(u.id);
                     }
+                    props.toggleFollowingProgress(false, u.id);
                   });
 
                 
 
               }}>Unfollow </button>
-              : <button onClick={() => {
-                
+              : <button disabled={props.followingInProgress.some(id=> id === u.id)} onClick={() => {
+                props.toggleFollowingProgress(true, u.id);
                 axios .post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
                   withCredentials:true,
                   headers: { 
@@ -70,6 +71,7 @@ let Users = (props) => {
                   if (response.data.resultCode === 0) {
                       props.follow(u.id);
                     }
+                    props.toggleFollowingProgress(false, u.id);
                   });
 
                 
