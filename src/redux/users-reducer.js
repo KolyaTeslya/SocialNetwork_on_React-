637b510fd9aa92923f9a2556dc0d3d5e1575ba5e
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-01-30 17:16:00 +0200

    thunk

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -65,8 +65,8 @@ const usersReducer = (state = initialSate, action) => {
   }
 };
 
-export const follow = (userId) => ({ type: FOLLOW, userId });
-export const unfollow = (userId) => ({ type: UNFOLLOW, userId });
+export const followSuccess = (userId) => ({ type: FOLLOW, userId });
+export const unfollowSuccess = (userId) => ({ type: UNFOLLOW, userId });
 export const setUsers = (users) => ({ type: SET_USERS, users });
 export const setCurrentPage = (currentPage) => ({
   type: SET_CURRENT_PAGE,
@@ -86,15 +86,42 @@ export const toggleFollowingProgress = (isFetching, userId) => ({
   userId,
 });
 
-export const getUsersThunkCreator = (currentPage, pageSize) => {
+export const getUsers = (currentPage, pageSize) => {
    return (dispatch) => {
     dispatch(toggleIsFetching(true));
 
     usersAPI.getUsers(currentPage, pageSize)
       .then((data) => {
+        dispatch(setCurrentPage(currentPage)); 
         dispatch(toggleIsFetching(false));
         dispatch(setUsers(data.items));
         dispatch(setTotalUsersCount(data.totalCount));
+      });
+  }
+}
+
+export const follow = (userId) => {
+   return (dispatch) => {
+    dispatch(toggleFollowingProgress(true, userId));
+    usersAPI.follow(userId)
+    .then((response) => {
+      if (response.data.resultCode === 0) {
+          dispatch(followSuccess(userId));
+        }
+        dispatch(toggleFollowingProgress(false, userId));
+      });
+  }
+}
+
+export const unfollow = (userId) => {
+   return (dispatch) => {
+    dispatch(toggleFollowingProgress(true, userId));
+    usersAPI.unfollow(userId)
+    .then((response) => {
+      if (response.data.resultCode === 0) {
+          dispatch(unfollowSuccess(userId));
+        }
+        dispatch(toggleFollowingProgress(false, userId));
       });
   }
 }
