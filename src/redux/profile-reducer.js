18ba8bc0f58fa8e42d51a32a25cac91e1a316f98
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-02-05 14:16:18 +0200

    add http put, and getStatus and updateStatus

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,8 +1,9 @@
-import { usersAPI } from "../api/api";
+import { profileAPI, usersAPI } from "../api/api";
 
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
+const SET_STATUS = "SET_STATUS";
 
 let initialSate = {
   posts: [
@@ -12,7 +13,8 @@ let initialSate = {
     { id: 4, message: "It's cool", likesCount: 17 },
   ],
   newPostText: "it-proger",
-  profile: null
+  profile: null,
+  status: ""
 };
 
 const profileReducer = (state = initialSate, action) => {
@@ -31,10 +33,15 @@ const profileReducer = (state = initialSate, action) => {
       }
     } 
     case UPDATE_NEW_POST_TEXT: {
-
       return {
         ...state,
         newPostText: action.newText
+      }
+    }
+    case SET_STATUS: {
+      return {
+        ...state,
+        status: action.status
       }
     }
     case SET_USER_PROFILE: {
@@ -50,12 +57,32 @@ const profileReducer = (state = initialSate, action) => {
 
 export const addPostActionCreator = () => ({ type: ADD_POST })
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
+export const setStatus = (status) => ({ type: SET_STATUS, status })
 
 export const getUserProfile = (userId) => (dispatch) => {
   usersAPI.getProfile(userId).then((response) => {
     dispatch(setUserProfile(response.data));
   });
 }  
+
+export const getStatus = (userId) => (dispatch) => {
+  profileAPI.getStatus(userId)
+  .then((response) => {
+    debugger;
+    dispatch(setStatus(response.data));
+  });
+}  
+
+export const updateStatus = (status) => (dispatch) => {
+  profileAPI.updateStatus(status)
+  .then((response) => {
+      if(response.data.resultCode === 0) {
+        dispatch(setStatus(status));
+      }
+  });
+}  
+
+
 export const updateNewPostTextActionCreator = (text) => ({
   type: UPDATE_NEW_POST_TEXT,
   newText: text,
