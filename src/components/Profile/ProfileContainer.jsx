44bc94508360733a880bee824aac5adb38cf547c
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-02-07 18:43:12 +0200

    add componentDidUpdate

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -16,7 +16,7 @@ class ProfileContainer extends React.Component {
   componentDidMount() {
     let userId = this.props.match.params.userId;
     if(!userId) { 
-      userId = 2;
+      userId = 27619;
     }
     this.props.getUserProfile(userId);
     this.props.getStatus(userId);
