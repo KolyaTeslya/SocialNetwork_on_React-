88cb16f6803d65acf8200a78350fec131d3a13ac
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-02-23 06:34:23 +0200

    add tests profile-rducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import { profileAPI, usersAPI } from "../api/api";
 const ADD_POST = "ADD-POST";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
 const SET_STATUS = "SET_STATUS";
+const DELETE_POST = "DELETE_POST";
 
 let initialSate = {
   posts: [
@@ -42,6 +43,12 @@ const profileReducer = (state = initialSate, action) => {
         profile: action.profile
       }
     }
+
+    case DELETE_POST:
+      return {
+        ...state, 
+        posts: state.posts.filter(p => p.id != action.postId)
+      }
     default:
         return state;
   }
@@ -50,6 +57,7 @@ const profileReducer = (state = initialSate, action) => {
 export const addPostActionCreator = (newPostText) => ({ type: ADD_POST, newPostText })
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
 export const setStatus = (status) => ({ type: SET_STATUS, status })
+export const deletePost = (postId) => ({ type: DELETE_POST, postId })
 
 export const getUserProfile = (userId) => (dispatch) => {
   usersAPI.getProfile(userId).then((response) => {
--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,25 +1,56 @@
-import profileReducer, { addPostActionCreator } from "./profile-reducer";
-import { ReactDOM } from "react-dom";
 import React from "react";
+import profileReducer, { addPostActionCreator, deletePost } from "./profile-reducer";
+import { ReactDOM } from "react-dom";
 
+let state = {
+  posts: [
+    { id: 1, message: "Hi, how are you?", likesCount: 12 },
+    { id: 2, message: "It's my first post", likesCount: 11 },
+    { id: 3, message: "Ok", likesCount: 14 },
+    { id: 4, message: "It's cool", likesCount: 17 }
+  ]
+};
 
 it('length of posts should be incremented', () => {
   // 1. test data
   let action = addPostActionCreator("it-proger.com");
-  let state = {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likesCount: 12 },
-      { id: 2, message: "It's my first post", likesCount: 11 },
-      { id: 3, message: "Ok", likesCount: 14 },
-      { id: 4, message: "It's cool", likesCount: 17 },
-    ]
-  };
+
   // 2. action
   let newState = profileReducer(state, action);
 
   // 3. expectation
    expect(newState.posts.length) .toBe(5);
+});
+
+it('message of new post should be correct', () => {
+  // 1. test data
+  let action = addPostActionCreator("it-proger.com");
+
+  // 2. action
+  let newState = profileReducer(state, action);
+
+  // 3. expectation
    expect(newState.posts[4].message) .toBe("it-proger.com");
 });
 
+it('after deleting length of messages should be decrement', () => {
+  // 1. test data
+  let action = deletePost(1);
 
+  // 2. action
+  let newState = profileReducer(state, action);
+
+  // 3. expectation
+   expect(newState.posts.length) .toBe(3);
+});
+
+it(`after deleting length should'nt be decrement if id is incorrect`, () => {
+  // 1. test data
+  let action = deletePost(1000);
+
+  // 2. action
+  let newState = profileReducer(state, action);
+
+  // 3. expectation
+   expect(newState.posts.length) .toBe(4);
+});
